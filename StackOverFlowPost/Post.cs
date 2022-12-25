871afa14ef8e0798cb7e6a437b6a6471d6c76fbd
musta <musta@DESKTOP-44RR6PQ>
2022-12-25 18:45:53 +0200

    Add project files.

---
+++ StackOverFlowPost/Post.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverFlowPost
+{
+    internal class Post
+    {
+        private string Title { get; set; }
+        private string Description { get; set; }
+        private DateTime Date { get; set; } = DateTime.Now;
+        private int votes { get; set; }
+
+        public Post(string title, string description)
+        {
+            if (string.IsNullOrEmpty(title) || string.IsNullOrEmpty(description))
+                throw new Exception("There's no post to be created.");
+
+            this.Title = title;
+            this.Description = description;
+        }
+
+        public string ShowPost()
+        {
+            return $"Title : {this.Title}\n" +
+                $"Post: {this.Description}.\n" +
+                $"Date Created: {this.Date}. \n" +
+                $"Number of Votes: {GetVotes()}";
+        }
+        public void VotesUp()
+        {
+            votes++;
+        }
+
+        public void VotesDown()
+        {
+            votes--;
+        }
+
+        public int GetVotes()
+        {
+            return votes;
+        }
+    }
+}
---
+++ StackOverFlowPost/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace StackOverFlowPost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter The Title of Your post:");
+            string Title1 = Console.ReadLine();
+            Console.Write("Descripe Your Topic: ");
+            string Description1 = Console.ReadLine();
+
+            Post post = new Post(Title1, Description1);
+            post.VotesUp();
+            post.VotesUp();
+            post.VotesUp();
+            post.VotesUp();
+            post.VotesDown();
+            post.VotesDown();
+     
+
+            Console.Write("Enter The Title of Your post:");
+            string Title2 = Console.ReadLine();
+            Console.Write("Descripe Your Topic: ");
+            string Description2 = Console.ReadLine();
+
+            Post post2 = new Post(Title2, Description2);
+            post2.VotesUp();
+            post2.VotesUp();
+            post2.VotesUp();
+            post2.VotesDown();
+
+            Console.WriteLine();
+            Console.WriteLine("Post 1: ");
+            Console.WriteLine(post.ShowPost());
+            Console.WriteLine();
+            Console.WriteLine("Post 2: ");
+            Console.WriteLine(post2.ShowPost());
+            Console.WriteLine();
+
+
+        }
+    }
+}
